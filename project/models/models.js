5e7d7a869a4213e7ed9038a8cadd898c61ec0ac1
felixacdc <pfmata01@gmail.com>
2016-10-29 00:02:39 -0600

    [modify] Curso MOOC: Node.JS. Quiz 7B. Sequelize.js y SQlite

--- project/models/models.js
+++ project/models/models.js
@@ -5,9 +5,10 @@ const Sequelize = require('sequelize');
 
 // Usar DDBB MySQL
 var sequelize = new Sequelize('MVCExpress', 'homestead', 'secret', {
-  host: '127.0.0.1',
+  host: '192.168.10.10',
   dialect: 'mysql'
 });
+/*var sequelize = new Sequelize('mysql://homestead:secret:33060/MVCExpress');*/
 
 // Importa la definicion de la tabla Quiz en quiz.js
 const Quiz = sequelize.import(path.join(__dirname, 'quiz'));
@@ -15,18 +16,24 @@ const Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; // exporta definicion de tabla Quizes
 
 // squelize.sync crea e inicializa tabla de preguntas en DB
-sequilize.sync().success(function() {
+Quiz.sync().then(function() {
     // success(..) ejecuta el manejador una vez creada la tabla
-    Quiz.count().success(function(count) {
+    Quiz.count().then(function(count) {
         if(count === 0) {
             Quiz.create({
                 pregunta: 'Capital de Italia',
                 respuesta: 'Roma'
-            }).success(function() {
+            }).then(function() {
                 console.log("Base de datos inicializada");
-            });            
+            }).catch(function(err){
+                console.log(`Error: ${err}`);
+            });         
         }
         
+    }).catch(function(err){
+        console.log(`Error: ${err}`);
     });
+}).catch(function(err){
+    console.log(`Error: ${err}`);
 });
 
