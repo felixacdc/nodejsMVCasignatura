5de286acd6eee4c748bd233a22cefccaad93d1f0
felixacdc <pfmata01@gmail.com>
2016-10-31 21:43:13 -0600

    [add] Curso MOOC: Node.JS. Quiz 11. Crear preguntas

--- project/controllers/quiz_controller.js
+++ project/controllers/quiz_controller.js
@@ -36,3 +36,12 @@ exports.answer = function (req, res) {
     
     res.render('quizes/answer', { quiz: req.quiz, respuesta });
 };
+
+// GET /quizes/new
+exports.new = function (req, res) {
+    var quiz = models.Quiz.build(
+        { pregunta: "Pregunta", respuesta: "Respuesta"}
+    );
+    
+    res.render("quizes/new", {quiz: quiz});
+}
