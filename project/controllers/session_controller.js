9c59b237e47c979041a03b61c642de9fca59a125
felixacdc <pfmata01@gmail.com>
2016-11-09 22:57:54 -0600

    [modify] Curso MOOC Node.JS. Proyecto Quiz 16. Autenticación y sesión

--- project/controllers/session_controller.js
+++ project/controllers/session_controller.js
@@ -5,3 +5,26 @@ exports.new = function(req, res) {
     
     res.render('sessions/new', {errors});
 };
+
+exports.create = function(req, res) {
+    var login = req.body.login;
+    var password = req.body.password;
+    
+    var userController = require('./user_controller');
+    userController.autenticar(login, password, function(error, user) {
+       if(error) {
+           req.session.errors = [{"message": "Se ha producido un error: " + error}];
+           res.redirect("/login");
+           return;
+       } 
+        
+        req.session.user = {id: user.id, username: user.username};
+        
+        res.redirect(req.session.redir.toString());
+    });
+};
+
+exports.destroy = function(req, res) {
+    delete req.session.user;
+    res.redirect(req.session.redir.toString());
+};
